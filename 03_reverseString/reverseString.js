a168dff18ba13c24e841893a6cad4e59868b3a4b
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-16 04:53:25 +0700

    03,04 practices done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('')
 };
+
+/* turn the string into an array by .split(), reverse it, join the string */
+
+
+console.log(reverseString('walle'))
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
+
+/* const reverseString = function(string) {
+    return string.split('').reverse().join('');
+   }; */
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = (...args) => {
+    const array = args[0];
+    const emptyArray = [];
 
+    array.forEach(item => {
+        if (!args.includes(item)) {
+            emptyArray.push(item);
+        }
+    })
+    return emptyArray
 };
 
+console.log(removeFromArray([1,2,3,4],3))
+
+
+
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; 
+
+
+
+
+let animals = ['cow', 'cat', 'dog', 'snake']
+let strings = 'is suuck'
+
+
+animals.forEach(animal => {
+    console.log(`${animal} ${strings}`)
+})
+ 
