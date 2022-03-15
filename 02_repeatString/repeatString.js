d9418a01b56761d120d98a17063a5342d2198bff
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-15 14:00:34 +0700

    repeat string check!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,29 +1,37 @@
-const repeatString = (string,num) => {
+const repeatString = (string, number) => {
+    if (number <= 0) return 'Number must be greater than 0'
+    let word = '';
+    for (let i = 0; i < number; i++) {
+            word = word + string; //code block to be executed
+    }
+    return word
+}
+
+
+
+console.log(repeatString('test ', 7));
+module.exports = repeatString;
+
+
+
+
+/* const repeatString = (string, num) => {
     if (string < 0) {
         return 'Error!'
     };
+    
     let word = '';
     for (let i = 0; i < num; i++) {
-        word += string;
+        word = word + string;
     }
-    return  word;
-};
+    return word;
+}; */
 
-
-
-// const repeatString = function(word, times) {
-    //     if (times < 0) return 'ERROR';
-    //     let string = '';
-    //     for (let i = 0; i < times; i++) {
-        //       string += word;
-        //     }
-        //     return string;
-        //   };
-        
-        
-  module.exports = repeatString;
-
-
-
-
-
+/* const repeatString = function (word, times) {
+    if (times <= 0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+    return string;
+}; */
