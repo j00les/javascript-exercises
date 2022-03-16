8080eb7b2f89165aefab65dd9da2022760918e75
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-16 10:08:38 +0700

    sumall exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,22 @@
 const sumAll = (first, last) => {
-    if(!Number.isInteger(first) || !Number.isInteger(last)) {
-        return "ERR-Motherfucking-OR"
-    
-      } if (first < 0 || last < 0) {
-        return "ERR-Motherfucking-OR"
-    
-      } if (first > last) {
-        
-      }
+  if (!Number.isInteger(first) || !Number.isInteger(last)) return "ERROR";
+  if (first < 0 || last < 0) return "ERROR";
+  if (first > last) {
+    const temp = first;
+    first = last;
+    last  = temp;
+  }
 
-};
+
+  let sum = 0; 
+  for (let i = first; i < last + 1; i++) {
+    sum += i;
+  }
+  return sum
+}
+
+console.log(sumAll(-10,[23,22]))
+     
 
 // Do not edit below this line
 module.exports = sumAll;
