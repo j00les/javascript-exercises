697c54f974d926cfe6a47d562bef5261cc517504
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-16 10:44:40 +0700

    temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,27 @@
-const ftoc = function() {
-
+const ftoc = (f) => {
+  return Math.round((f - 32) * .5556 * 10)  / 10;
 };
 
-const ctof = function() {
-
+const ctof = (c) => {
+  return Math.round(((c * 1.8) + 32) * 10) / 10;
 };
+
+console.log(ftoc(100))
+console.log(ctof(-10))
+
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
+
+/* const ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+ };
+ 
+ const ctof = function(c) {
+   return Math.round(((c * 9/5) + 32) * 10) / 10;
+ }; */
+
