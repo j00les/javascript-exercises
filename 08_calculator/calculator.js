55c137f0a2aae6f9a64fb7286779ae4a651bdf23
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-31 17:05:40 +0700

    Fundamentals 5 all exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,25 @@
-const add = function(addThis) {
-	return addThis + addThis
+const add = (addA, addB ) => 	addA + addB;
+
+
+const subtract = (subtractA, subtractB) => subtractA - subtractB;
+
+const sum = (array) =>  array.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+const multiply = (array) => array.reduce((previousValue, currentValue) => previousValue * currentValue)
+
+
+const power = (a, b) => Math.pow(a, b)
+
+
+const factorial = (n) => {
+  if (n === 0) return 1;
+  let num = 1;
+  for (let i = n; i > 0; i--) {
+    num *= i
+  }
+  return num;
 };
 
-const subtract = function(a,b) {
-	return a - b
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
